c58fe116db38c7643b0b7a491f8eb8c6facb49a5
shseok <shseok0674@gmail.com>
2023-01-26 23:25:15 +0900

    Resolve vercel error

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,9 @@
 // const $histories = document.querySelector('#histories');
 import setRecsWithHistories from './createHistory';
-import { serverAddress } from './settings';
+// import { serverAddress } from './settings';
 
 const $loading = document.querySelector('#loading');
+const serverAddress = 'https://us-central1-zandi-203bf.cloudfunctions.net/expressapi/histories';
 let loading = false;
 
 const getHistoriesFromBackend = async () => {
